dc2ca0483be38db67431582fc1d922f2d39d762e
Marcstro <marcus.strom.90@gmail.com>
2019-09-03 22:19:40 +0200

    Allt pa formularet finns nu

--- src/java/Person.java
+++ src/java/Person.java
@@ -20,6 +20,36 @@ public class Person {
     private String message;
     
     private String namn;
+    private String kon;
+    private int alder;
+    private String jobb;
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+
+    public String getJobb() {
+        return jobb;
+    }
+
+    public void setJobb(String jobb) {
+        this.jobb = jobb;
+    }
+    
+    
+
+    public String getKon() {
+        return kon;
+    }
+
+    public void setKon(String kon) {
+        this.kon = kon;
+    }
+    
 
     public String getMessage() {
         return message;
@@ -47,6 +77,7 @@ public class Person {
         else{
             message="Fel namn!";
         }
+        message+=", namn: " + namn + ", alder: " + alder + ", kon: " + kon + ", jobb: " + jobb;
     }
     
     
