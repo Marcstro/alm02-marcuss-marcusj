43b8964caa9af8a399976645fc22fc23a697dc48
Marcstro <marcus.strom.90@gmail.com>
2019-09-04 14:45:59 +0200

    Tester finns för ALLTING nu

---
+++ test/testPerson.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marcu
+ */
+public class testPerson {
+    
+    @Test
+    public void testgenerateMessage(){
+        Person p = new Person();
+        p.setAlder(50);
+        p.setNamn("sandra");
+        p.setKon("woman");
+        p.setJobb("programmerare");
+        
+        for(int x=0; x<100; x++){
+            p.generateMessage();
+            assertTrue((p.getMessage().contains("Det var ett fint namn")) ||
+                    (p.getMessage().contains("Du ar i livets gyllende ar, vad")) ||
+                    (p.getMessage().contains("Vilket fint h")) ||
+                    (p.getMessage().contains("Du ar en del av fr")));
+                    
+        }
+        for(int x=0; x<100; x++){
+            p.generateMessage();
+            assertFalse((p.getMessage().contains("Ett ordentligt ")) ||
+                    (p.getMessage().contains("oj du var ga")) ||
+                    (p.getMessage().contains("HORSE IS NOT A GE")) ||
+                    (p.getMessage().contains("Du skapar framtidens l")));
+                    
+        }
+    }
+    
+}
--- test/testRandomGenerator.java
+++ test/testRandomGenerator.java
@@ -15,10 +15,25 @@ public class testRandomGenerator {
     
     RandomGenerator rg = new RandomGenerator();
     
+    public void testwhatFactorToConsider(){
+        for(int x=0; x<100; x++){
+            assertTrue((rg.whatFactorToConsider()>-1) && (rg.whatFactorToConsider() < 4));
+        }
+    }
+    
     @Test
     public void testgetOneRandomNumber(){
         for(int x=0; x<100; x++){
-            assertTrue(rg.whatFactorToConsider() < 4 && rg.whatFactorToConsider() > -1);
+            assertTrue(rg.getOneRandomNumber() < 101 && rg.getOneRandomNumber() > -1);
+        }
+    }
+    @Test
+    public void testgetTwoRandomNumbers(){
+        for(int x=0; x<100; x++){
+            
+            int[] a = rg.getTwoRandomNumbers();
+            assertTrue((a[0]>-1) && (a[0] < 101) && (a[1]>-1) && (a[1] < 101));
+            
         }
     }
     
